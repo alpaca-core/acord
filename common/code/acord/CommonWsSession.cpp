1eacb954646e453abb5c149d0830cc7cffb7f271
Borislav Stanimirov <b@ibob.bg>
2025-02-02 07:35:36 +0200

    chore: move common ws session code to common, ref #5

--- common/code/acord/CommonWsSession.cpp
+++ common/code/acord/CommonWsSession.cpp
@@ -3,8 +3,112 @@
 //
 #include "CommonWsSession.hpp"
 
+#include <ac/jalog/Log.hpp>
+
 namespace acord {
 
 CommonWsSession::~CommonWsSession() = default;
+
+void CommonWsSession::wsClosed() {
+    AC_JALOG(Info, "Session closed");
+    m_dispatch.readStream->close();
+    m_dispatch.writeStream->close();
+}
+
+void CommonWsSession::tryWriteToDispatch() {
+    while (true) {
+        if (m_received.empty()) {
+            return;
+        }
+        auto res = m_dispatch.writeStream->write(m_received.front(), [&] {
+            return [this, pl = shared_from_this()] {
+                postWSIOTask([this]() {
+                    tryWriteToDispatch();
+                });
+            };
+        });
+        if (res.blocked()) break;
+        if (res.closed()) {
+            wsClose();
+            return;
+        }
+        assert(res.success());
+        m_received.pop_front();
+    }
+}
+
+template <typename T>
+void CommonWsSession::received(itlib::span<T> span) {
+    auto& frame = m_received.emplace_back();
+
+    try {
+        if constexpr (std::is_same_v<T, char>) {
+            m_cvt.jsonBufToAc(frame, {span.data(), span.size()});
+        }
+        else {
+            m_cvt.cborBufToAc(frame, {span.data(), span.size()});
+        }
+    }
+    catch (std::exception& ex) {
+        AC_JALOG(Error, "Error reading frame: ", ex.what());
+        wsClose();
+        return;
+    }
+
+    if (m_received.size() == 1) {
+        tryWriteToDispatch();
+    }
+}
+
+void CommonWsSession::wsReceivedText(itlib::span<char> text) {
+    received(text);
+}
+
+void CommonWsSession::wsReceivedBinary(itlib::span<uint8_t> binary) {
+    received(binary);
+}
+
+void CommonWsSession::tryReadFromDispatch() {
+    if (m_sending) return;
+    auto& buf = m_sending.emplace();
+
+    ac::Frame frame;
+    auto res = m_dispatch.readStream->read(frame, [this] {
+        return [this, pl = shared_from_this()] {
+            postWSIOTask([this]() {
+                tryReadFromDispatch();
+            });
+        };
+    });
+
+    if (res.success()) {
+        m_cvt.fromAcFrame(buf, frame);
+        doSend();
+        return;
+    }
+
+    m_sending.reset();
+
+    if (res.closed()) {
+        wsClose();
+    }
+
+    assert(res.closed() || res.blocked());
+}
+
+void CommonWsSession::doSend() {
+    assert(!!m_sending);
+    if (m_sending->text()) {
+        wsSend(m_sending->textBuf);
+    }
+    else {
+        wsSend(m_sending->binaryBuf);
+    }
+}
+
+void CommonWsSession::wsCompletedSend() {
+    m_sending.reset();
+    tryReadFromDispatch();
+}
 
 } // namespace acord
--- common/code/acord/CommonWsSession.hpp
+++ common/code/acord/CommonWsSession.hpp
@@ -7,6 +7,8 @@
 #include <ac/frameio/StreamEndpoint.hpp>
 #include <fishnets/WebSocketSession.hpp>
 #include <astl/shared_from.hpp>
+#include <deque>
+#include <optional>
 
 namespace acord {
 
@@ -15,6 +17,22 @@ protected:
     ~CommonWsSession();
     FrameCvt m_cvt = FrameCvt::json();
     ac::frameio::StreamEndpoint m_dispatch;
+
+    std::deque<ac::Frame> m_received;
+    std::optional<acord::Frame> m_sending;
+
+    void wsClosed() override; // not final (likely to be overridden further)
+
+    void tryWriteToDispatch();
+
+    template <typename T>
+    void received(itlib::span<T> span);
+
+    void wsReceivedText(itlib::span<char> text) final override;
+    void wsReceivedBinary(itlib::span<uint8_t> binary) final override;
+    void tryReadFromDispatch();
+    void doSend();
+    void wsCompletedSend() final override;
 };
 
 } // namespace acord
